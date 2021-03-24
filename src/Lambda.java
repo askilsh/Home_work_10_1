638541762ca0063110be31a76610ec47db100e03
ILSHAT <askils@mail.com>
2021-03-24 18:55:00 +0300

    comm

--- src/Lambda.java
+++ src/Lambda.java
@@ -2,42 +2,39 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lambda {
-    private final int size = 10;
+    private static final int SIZE = 10;
 
     public static void main(final String[] argc) {
-        final int i = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите модификатор... Пожалуйста...");
         int mfr = scanner.nextInt();
         Lambda lambda = new Lambda();
         int[] fstMass;
-        fstMass = lambda.getFilledArray(lambda.size, mfr, Integer::sum);
+        fstMass = lambda.getFilledArray(SIZE, mfr, Integer::sum);
         int[] scdMass;
-        scdMass = lambda.getFilledArray(lambda.size, mfr, (inx, mod) -> {
-            return inx * mod;
-        });
+        scdMass = lambda.getFilledArray(SIZE, mfr, (inx, mod) -> inx * mod);
         int[] thdMass;
-        thdMass = lambda.getFilledArray(lambda.size, mfr, (inx, mod) -> {
+        thdMass = lambda.getFilledArray(SIZE, mfr, (inx, mod) -> {
             if ((inx % 2) == 0) {
                 return inx / 2 + mod;
             } else {
                 return (inx * inx) - mod;
             }
         });
-        int[] degreeMass;
-        degreeMass = lambda.getFilledArray(lambda.size, mfr, (inx, mod) -> {
-            int puff = inx;
+        int[] degreeMass;  // Лямбда, которая возводит индекс массива в степень модификатора
+        degreeMass = lambda.getFilledArray(SIZE, mfr, (inx, mod) -> { // буфер, чтобы помнить значение индекса
+            int puff = inx; // Умножаем значение индекса на себя, пока степень больше 1
             for (; mod > 1; mod--) {
                 inx = inx * puff;
             }
             return inx;
         });
-        int[] factMass;
-        factMass = lambda.getFilledArray(lambda.size, mfr, (inx, mod) -> {
+        int[] factMass; // Лямбда, которая возводит значение индекса в факториал и умножает на модификатор
+        factMass = lambda.getFilledArray(SIZE, mfr, (inx, mod) -> { // Факториал 0 равен 1
             if (inx == 0) {
                 inx = 1;
-            }
-            int puff = inx;
+            } // Буфер, чтобы помнить значение индекса
+            int puff = inx; // Умножаем значение индекса на значение индекса - 1 в цикле, пока оно больше 1
             for (; inx > 1; inx--) {
                 puff = puff * (inx - 1);
             }
